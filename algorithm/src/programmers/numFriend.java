51a2b227a24fec7ec2875b4e38e9813ab4f15851
heopaka <gjtmdwns0815@naver.com>
2023-03-09 20:26:47 +0900

    20230309 programmers numFriend modify

--- algorithm/src/programmers/numFriend.java
+++ algorithm/src/programmers/numFriend.java
@@ -1,27 +1,32 @@
 package programmers;
 import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 class Solution5 {
     public String solution(String X, String Y) {
         String answer = "";
-        ArrayList<Character> list = new ArrayList<Character>();
-        ArrayList<String> strX = new ArrayList<String>();
-        ArrayList<String> strY = new ArrayList<String>();
         
-        for(int i=0;i<X.length();i++) strX.add(String.valueOf(X.charAt(i)));
-        for(int i=0;i<Y.length();i++) strY.add(String.valueOf(Y.charAt(i)));
+        String[] split1 = X.split("");
+        String[] split2 = Y.split("");
+        
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> strX = new ArrayList<String>(Arrays.asList(split1));
+        ArrayList<String> strY = new ArrayList<String>(Arrays.asList(split2));
         
         for(int i=0;i<strX.size();i++){
-            for(int j=0;j<strY.size();j++){
-                if(strX.get(i).equals(strY.get(j))) {
-                    list.add(X.charAt(i));
-                    strX.remove(i);
-                    break;
-                }
+            String attr = strX.get(i);
+            if(strY.contains(attr)) {
+                list.add(attr);
+                strY.remove(attr);
             }
         }
+        
         if(list.isEmpty()) answer = "-1";
         else {
-            for(int i=0;i<list.size();i++) answer += String.valueOf(list.get(i));
+            Collections.sort(list,Collections.reverseOrder());
+            for(int i=0;i<list.size();i++) answer += list.get(i);
+            if(answer.replaceAll("0","").isEmpty()) answer = "0";
         }
         return answer;
     }
