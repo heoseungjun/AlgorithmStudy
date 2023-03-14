a1d22e4af7c3857af19132ef44c0035a3a6d4f66
heopaka <gjtmdwns0815@naver.com>
2023-03-14 12:32:21 +0900

    20230314 programmers keypad solving

---
+++ algorithm/src/programmers/keypad.java
@@ -0,0 +1,14 @@
+package programmers;
+class Solution10 {
+	 public String solution(int[] numbers, String hand) {
+		 String answer ="";
+		 int[][] keys = {{1,2,3},{4,5,6},{7,8,9},{0,0,0}};
+		 return answer;
+	 }
+}
+
+public class keypad {
+	public static void main(String[] args) {
+
+	}
+}
