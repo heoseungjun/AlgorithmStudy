cc84dd0955af68429af4c61412d3cd242d9ccba4
heopaka <gjtmdwns0815@naver.com>
2023-03-17 09:57:05 +0900

    20230317 programmers recommendId solving

---
+++ algorithm/src/programmers/recommendId.java
@@ -0,0 +1,27 @@
+package programmers;
+class Solution11 {
+    public String solution(String new_id) {
+        String answer = "";
+        // 1단계 : 대문자를 소문자로 변경
+        answer = new_id.toLowerCase();
+        // 2단계 : 불필요한 특수기호 제거
+        answer = answer.replaceAll("[^-_.a-z0-9]","");
+        // 3단계 : 마침표가 연속으로 나온다면 하나로 변경
+        while(answer.indexOf("..") != -1) answer = answer.replace("..",".");
+        // 4단계 : 아이디의 처음 혹은 끝에 있는 마침표가 있다면 제거
+        answer = answer.replaceAll("^[.]|[.]$","");
+        // 5단계 : 아이디가 빈 문자열인지 확인, 비어있으면 a 입력
+        if(answer.isEmpty()) answer += "a";
+        // 6단계 : 아이디가 15글자를 초과했다면 1~15째 글자만 남기고 지우기
+        if(answer.length()>15) answer = answer.substring(0,15);
+        // 7단계 : 아이디 길이가 2자 이하인지 확인, 이하라면 현재 문자열 반복
+        while(answer.length()<3) answer += answer.charAt(answer.length()-1);
+        return answer;
+    }
+}
+
+public class recommendId {
+	public static void main(String[] args) {
+		Solution11 sol = new Solution11();
+	}
+}
