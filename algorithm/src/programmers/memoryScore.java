1a23dd6b25d7f03f8227a907054296eeadcd9880
heopaka <gjtmdwns0815@naver.com>
2023-04-01 08:44:37 +0900

    20230401 programmers memoryScore modify

--- algorithm/src/programmers/memoryScore.java
+++ algorithm/src/programmers/memoryScore.java
@@ -1,5 +1,6 @@
-class Solution {
-    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+package programmers;
+class Solution17 {
+	public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
         for(int i=0;i<photo.length;i++){
             for(int j=0;j<photo[i].length;j++){
@@ -10,4 +11,19 @@ class Solution {
         }
         return answer;
     }
+}
+
+public class memoryScore {
+	public static void main(String[] args) {
+		Solution17 sol = new Solution17();
+		String[] name = {"may", "kein", "kain", "radi"};
+		int[] yearning = {5, 10, 1, 3};
+		String[][] photo = {
+				{"may", "kein", "kain", "radi"},
+				{"may", "kein", "brin", "deny"},
+				{"kon", "kain", "may", "coni"}
+		};
+		
+		for(int i=0;i<sol.solution(name,yearning,photo).length;i++) System.out.println(sol.solution(name,yearning,photo)[i]);
+	}
 }
