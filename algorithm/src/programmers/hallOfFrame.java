b4c2dfb528cf257abcb78a8a700154354cafa6eb
heopaka <gjtmdwns0815@naver.com>
2023-04-07 15:29:58 +0900

    20230407 programmers hallOfFrame solving

---
+++ algorithm/src/programmers/hallOfFrame.java
@@ -0,0 +1,22 @@
+package programmers;
+import java.util.ArrayList;
+import java.util.Collections;
+class Solution19 {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        ArrayList<Integer> rank = new ArrayList<Integer>();
+        for(int i=0;i<score.length;i++){
+            rank.add(score[i]);
+            Collections.sort(rank);
+            if(k>=i) rank.remove(0);
+            answer[i] = rank.get(0);
+        }
+        return answer;
+    }
+}
+
+public class hallOfFrame {
+	public static void main(String[] args) {
+		Solution19 sol = new Solution19();
+	}
+}
