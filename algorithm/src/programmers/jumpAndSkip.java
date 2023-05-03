1d655a51b861d7a0f0d2ac3908474e40d16bef24
heopaka <gjtmdwns0815@naver.com>
2023-05-03 12:28:29 +0900

    20230503 programmers jumpAndSkip solved

--- algorithm/src/programmers/jumpAndSkip.java
+++ algorithm/src/programmers/jumpAndSkip.java
@@ -1,13 +1,11 @@
 package programmers;
-import java.util.*;
 
 class Solution25 {
-    public int solution(int n) {
-        int ans = 0;
-        while(n/2 >= 1) {
-            n /= 2;
-            System.out.println(n);
-            ans+=1;
+	public int solution(int n) {
+        int ans = 1;
+        while(n>1){
+            if(n%2 == 1) ans++;
+            n = n/2;
         }
         return ans;
     }
@@ -15,6 +13,7 @@ class Solution25 {
 public class jumpAndSkip {
 	public static void main(String[] args) {
 		Solution25 sol = new Solution25();
-		System.out.println("가장 효율적인 건전지 사용량 : "+sol.solution(5));
+		System.out.println("n이 5일 때 가장 효율적인 건전지 사용량 : "+sol.solution(5));
+		System.out.println("n이 5000일 때 가장 효율적인 건전지 사용량 : "+sol.solution(5000));
 	}
 }
