eea034335ef73d360a287294f234e18461a9c903
heopaka <gjtmdwns0815@naver.com>
2023-05-22 07:19:49 +0900

    20230522 programmers landGame solved

--- algorithm/src/programmers/landGame.java
+++ algorithm/src/programmers/landGame.java
@@ -1,18 +1,17 @@
 package programmers;
+import java.lang.Math;
 class Solution34 {
-	int solution(int[][] land) {
-        int answer = 0, tmp = 0, pre = 0;
+    int solution(int[][] land) {
+        int answer = 0;
+        for(int i=1;i<land.length;i++){
+            land[i][0] += Math.max(land[i-1][1],Math.max(land[i-1][2],land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0],Math.max(land[i-1][2],land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][2]));
+        }
         
-        for(int i=0;i<land.length;i++){
-            int max = land[i][0];
-            for(int j=1;j<land[0].length;j++){
-                if(max<land[i][j] && j!=pre) {
-                    max = land[i][j];
-                    tmp = j;
-                }
-            }
-            pre = tmp;
-            answer += max;
+        for(int i=0;i<4;i++){
+            answer = Math.max(answer,land[land.length-1][i]);
         }
         return answer;
     }
@@ -21,9 +20,9 @@ public class landGame {
 	public static void main(String[] args) {
 		Solution34 sol = new Solution34();
 		int[][] land = {
-				{1,2,3,5},
-				{5,6,7,8},
-				{4,3,2,1}
+				{1,2,3,4},
+				{6,7,4,5},
+				{3,9,5,7}
 		};
 		System.out.println("땅따먹기에서 얻은 최고 점수 : "+sol.solution(land));
 	}
