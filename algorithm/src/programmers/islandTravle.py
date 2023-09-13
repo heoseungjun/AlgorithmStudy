feff6a990654f00e4773471fb0c0673de2e47362
heopaka <gjtmdwns0815@naver.com>
2023-09-14 08:32:46 +0900

    2023 programmers islandTravle solving

--- algorithm/src/programmers/islandTravle.py
+++ algorithm/src/programmers/islandTravle.py
@@ -2,27 +2,27 @@ def solution(maps):
     answer = []
     N,M = len(maps),len(maps[0])
     direct = [[0,1],[1,0],[0,-1],[-1,0]]
-    V = [[0 for _ in range(M)] for _ in range(N)]
+    V = [[0]*M for _ in range(N)]
     
     for y in range(N) :
         for x in range(M) :
-            if maps[y][x] != "X" :
-                if V[y][x] == 1 : continue
-                V[y][x] = 1
-                nx,ny = x,y
-                tmp = int(maps[y][x])
-                for k in range(4) :
-                    cx,cy = nx+direct[k][0],ny+direct[k][1]
-                    while 0<=cx<M and 0<=cy<N and maps[cy][cx]!="X" and V[cy][cx]==0:
-                        nx,ny = cx,cy
-                        cx += direct[k][0]
-                        cy += direct[k][1]
-                        V[ny][nx] = 1
-                        tmp += int(maps[ny][nx])
-                        print(ny,nx)
-                answer.append(tmp)
+            if maps[y][x] != "X" and not V[y][x]:
+                answer.append(bfs(maps,y,x,N,M,V,direct))
                         
-    if len(answer) == 0 : return [-1]           
-    else : 
-        answer.sort()
-        return answer
+    if len(answer) : return sorted(answer)         
+    else : return [-1]
+
+def bfs(maps,y,x,N,M,V,direct) :
+    V[y][x] = 1
+    Q = [(y,x)]
+    tmp = 0
+    while Q :
+        ny,nx = Q.pop()
+        tmp += int(maps[ny][nx])
+        for d in direct :
+            a,b = d
+            if -1<nx+a<M and -1<ny+b<N and maps[ny+b][nx+a]!="X" and not V[ny+b][nx+a]:
+                ny += b; nx += a
+                V[ny][nx] = 1
+                Q.append((ny,nx))
+    return tmp
