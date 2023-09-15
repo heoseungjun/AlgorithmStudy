f45024f5f5e329dab6f0386620c8d542c67e2fc8
heopaka <gjtmdwns0815@naver.com>
2023-09-15 10:43:46 +0900

    20230915 programmers islandTravle solved

--- algorithm/src/programmers/islandTravle.py
+++ algorithm/src/programmers/islandTravle.py
@@ -21,8 +21,16 @@ def bfs(maps,y,x,N,M,V,direct) :
         tmp += int(maps[ny][nx])
         for d in direct :
             a,b = d
-            if -1<nx+a<M and -1<ny+b<N and maps[ny+b][nx+a]!="X" and not V[ny+b][nx+a]:
-                ny += b; nx += a
-                V[ny][nx] = 1
-                Q.append((ny,nx))
+            cy,cx = ny,nx
+            if -1<cx+a<M and -1<cy+b<N and maps[cy+b][cx+a]!="X" and not V[cy+b][cx+a]:
+                cy += b; cx += a
+                V[cy][cx] = 1
+                Q.append((cy,cx))
     return tmp
+
+maps = ["X591X","X1X5X","X231X", "1XXX1"]
+
+for i in range(len(maps)) :
+    print(maps[i])
+
+print("\n무인도와 무인도에서 얻을 수 있는 식량 : ",solution(maps))
